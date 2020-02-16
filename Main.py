6cdc15b8b6ecd7dc5a52d86d062d061f542d2406
bcote94 <47681284+bcote94@users.noreply.github.com>
2020-02-16 12:13:36 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,13 +10,10 @@ from ML.Predict import Stock_Predict
 
 start_date = '2011-01-01'
 end_date = '2019-10-15'
-train_max = '2019-01-01'
-
+train_max = '2019-01-01' #End of training period
 tsla = 'TSLA'
-##YTD ML Profits:  $121 per share
-##YTD DCA Profits: $-339 (loss) per share
 
-res = Stock_Predict(start_date,end_date,train_max,amzn).predict()
+res = Stock_Predict(start_date,end_date,train_max,tsla).predict()
 print("Test Set Results for Model:")
 print("Profits from ML Model: $",res[1])
 print("\nProfits from Dollar Cost Averaging: $",res[2],'\n')
