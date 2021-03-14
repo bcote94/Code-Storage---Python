8669acad59f3c7a7905ac2bef514b9289ed03574
Leigh Cote <leigh@pop-os.localdomain>
2021-03-14 00:20:41 -0700

    Remove ipynb

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='market_model',
-      version='0.3',
+      version='0.3.1',
       description='main package',
       license='MIT',
       packages=['market_learner'],
